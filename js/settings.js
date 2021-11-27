2bd86ac0f9d8c9b94066a03278996c010a90a9ad
tedyted26 <tedyted26@hotmail.com>
2021-11-27 21:28:04 +0100

    intento de js 2

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,12 @@
+$(document).ready(function() {
+
+    document.querySelector("#name").disabled="True";
+    document.querySelector("#email").disabled="True";
+
+
+});
+
+
 function load_pic(){
     let url_nueva = document.getElementById("pic_url").value;
 
@@ -8,6 +17,40 @@ function load_pic(){
         $('.close').click();
     }
     else {
-        console.log("slfdd")
+        console.log("Not the right format")
+    }
+}
+
+function change_name(){
+    if(document.querySelector("#name").disabled=="True"){
+        document.querySelector("#name").disabled="False";
+        document.querySelector(".boton_nombre").value="Guardar";
+    }
+    else{
+        //save data
+        document.querySelector("#name").disabled="True";
+    }
+}
+
+function change_mail(){
+    if(document.querySelector("#email").disabled=="True"){
+        document.querySelector("#email").disabled="False";
+        document.querySelector(".boton_email").value="Guardar";
+    }
+    else{
+        //save data
+        document.querySelector("#email").disabled="True";
+    }
+}
+
+function save_pass(){
+    //check if current password matches
+
+    if(document.querySelector("#new_password").value==document.querySelector("#password_confirmation").value){
+        //change passwords
+    }
+    else{
+        //save data
+        console.log("Both passwords must match")
     }
 }
